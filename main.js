3f447fc2f8537268ba89a97dd3c792fa9ac8cd7e
ale.araujo.vianna <ale.araujo.vianna@gmail.com>
2025-04-02 04:39:33 +0000

    recebendo valor da % ao clicar no botao

--- main.js
+++ main.js
@@ -1,4 +1,24 @@
 let bill = 0
-let tip = 0
+let tippercentage = 0
 let numberOfpeople = 0
+let buttonselected = null
 
+function receivebillvalue() {
+    bill = document.querySelector("#bill").valueasnumber
+
+}
+
+function receivenumberofvalue() {
+    numberOfpeople = document.querySelector("#people").valueasnumber
+}
+
+function receivetippercentagevalue(value) {
+    tippercentage = value / 100
+
+    if(buttonselected !== null) {
+        buttonselected.classList.remove("button-selected")
+    }
+
+    buttonselected = document.querySelector(´#button-${value}´)
+    buttonselected.classList.add("button-selected")
+}
